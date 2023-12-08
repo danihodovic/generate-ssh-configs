3bf13ad30c6ea8bf0e54ebfb261df98a59edd921
Dimosthenis Schizas <dimos@hackthebox.eu>
2023-12-08 13:04:55 +0200

    Loop through IPs get first public

--- digital_ocean.go
+++ digital_ocean.go
@@ -1,61 +1,65 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"html/template"
-	"os"
+    "context"
+    "fmt"
+    "html/template"
+    "os"
 
-	"github.com/digitalocean/godo"
-	"golang.org/x/oauth2"
+    "github.com/digitalocean/godo"
+    "golang.org/x/oauth2"
 )
 
 const tmpl = `
 {{ range $_, $d := .Instances -}}
 Host {{ $.Prefix }}{{ $d.Name }}
-  HostName {{ ( index $d.Networks.V4 1).IPAddress }}
-{{ end}}
+{{ range $_, $n := $d.Networks.V4 -}}
+{{ if eq $n.Type "public" }}  HostName {{ $n.IPAddress }}
+{{ break -}}
+{{ end -}}
+{{ end -}}
+{{ end -}}
 
 Host {{ .Prefix }}*
   User {{ .User }}
 `
 
 func generateDigitalOcean(prefix string) {
-	tokenSource := &TokenSource{}
-	oauthClient := oauth2.NewClient(context.Background(), tokenSource)
-	client := godo.NewClient(oauthClient)
+    tokenSource := &TokenSource{}
+    oauthClient := oauth2.NewClient(context.Background(), tokenSource)
+    client := godo.NewClient(oauthClient)
 
-	droplets, _, err := client.Droplets.List(context.TODO(), &godo.ListOptions{PerPage: 200})
-	checkErr(err)
+    droplets, _, err := client.Droplets.List(context.TODO(), &godo.ListOptions{PerPage: 200})
+    checkErr(err)
 
-	templateData := struct {
-		Prefix    string
-		User      string
-		Instances []godo.Droplet
-	}{
-		Prefix:    prefix,
-		User:      sshUser,
-		Instances: droplets,
-	}
+    templateData := struct {
+        Prefix    string
+        User      string
+        Instances []godo.Droplet
+    }{
+        Prefix:    prefix,
+        User:      sshUser,
+        Instances: droplets,
+    }
 
-	t := template.Must(template.New("tmpl").Parse(tmpl))
-	err = t.Execute(os.Stdout, templateData)
-	checkErr(err)
+    t := template.Must(template.New("tmpl").Parse(tmpl))
+    err = t.Execute(os.Stdout, templateData)
+    checkErr(err)
 }
 
 type TokenSource struct{}
 
 func (t *TokenSource) Token() (*oauth2.Token, error) {
-	envVar := "DIGITAL_OCEAN_TOKEN"
+    envVar := "DIGITAL_OCEAN_TOKEN"
 
-	envToken := os.Getenv(envVar)
-	if envToken == "" {
-		fmt.Printf("Please provide a personal access token as %s\n", envVar)
-		os.Exit(1)
-	}
+    envToken := os.Getenv(envVar)
+    if envToken == "" {
+        fmt.Printf("Please provide a personal access token as %s\n", envVar)
+        os.Exit(1)
+    }
 
-	token := &oauth2.Token{
-		AccessToken: envToken,
-	}
-	return token, nil
+    token := &oauth2.Token{
+        AccessToken: envToken,
+    }
+    return token, nil
 }
